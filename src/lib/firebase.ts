fe10c1c89654fa4a35815b769ba1271839fe8207
Othman AlRahmat <55173396+othmanalmsaeed@users.noreply.github.com>
2026-03-26 09:40:59 +0300

    Create firebase initialization file

---
+++ src/lib/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyD0lvPZbrfOpuYtwJJ78rHYyDkqHKqHTRQ",
+  authDomain: "wpl-999.firebaseapp.com",
+  projectId: "wpl-999",
+  storageBucket: "wpl-999.firebasestorage.app",
+  messagingSenderId: "186771068322",
+  appId: "1:186771068322:web:47a76e3a27505b89250efb"
+};
+const app = initializeApp(firebaseConfig);
+export default app;
